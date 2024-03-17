8d2f6572de2d1a8d1724ee2a1f685092d5e93eac
free1139 <free1139@163.com>
2024-03-17 08:58:34 +0000

    fix publish shell

--- cmd/supd/debug.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo $PRJ_ROOT
-./publish.sh
-sudo PRJ_ROOT=$PRJ_ROOT ./supd
--- cmd/supd/publish.sh
+++ cmd/supd/publish.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+target_dir="publish/supd"
+rm -rf $target_dir
+mkdir -p $target_dir
 
 go build -ldflags="-s -w"||exit 0
 
-echo "Publish done"
+cp -rf supd $target_dir
+cp -rf supc $target_dir 
+cp -rf script $target_dir 
+cp -rf etc $target_dir
+cp -rf setup.sh $target_dir
+
+echo "Publish to $target_dir done"
